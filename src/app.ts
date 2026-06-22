9668131b9d2b82becd11c55a602122b8bc265013
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-22 17:48:34 +0600

    added necessary middleware to my application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express, { type Application, type Request, type Response } from "express"
 import { userRouter } from "./modules/user/user.routes";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import config from "./config/config";
 
 const app : Application = express();
 
 //? here we will write the middlewares that we needed
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(cors({
+    origin : config.app_url,
+    credentials: true
+}))
 
 
 app.get("/", (req :Request, res : Response)=>{
