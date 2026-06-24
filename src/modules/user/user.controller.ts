726bc43d49493b7b0b92ebf5c9123463cf33ed38
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-25 00:35:01 +0600

    added higher order function for registering user and fetching all the user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,42 +1,36 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import userServices from "./user.services";
 import sendResponse from "../../utility/sendResponse";
 import status from "http-status"
+import catchAsync from "../../utility/catchAsync";
+import sendResponse2 from "../../utility/sendResponse2";
 
 
-const getAllUser = async (req: Request, res: Response) => {
-   try{
 
-        const users = await userServices.getAllUsersFromDB();
 
-        return sendResponse(res, status.OK, true, "User Information fetched successfully", users);
+const getAllUser = catchAsync(async(req : Request, res : Response, next : NextFunction)=>{
+    const users = await userServices.getAllUsersFromDB();
 
-   }catch(err){
-    
-    console.error(err);
-    const errorMessage = err instanceof Error ? err.message : "Something went wrong";
-    return sendResponse(res, status.INTERNAL_SERVER_ERROR, false, errorMessage);
+    return sendResponse2(res, {
+        success : true,
+        statusCode : status.OK,
+        message : "User Information fetched successfully!",
+        data : users
+    })
+})
 
-   }
-}
+const registerUser = catchAsync(async(req : Request, res : Response, next : NextFunction)=>{
+    const payLoad = req.body;
 
-const registerUser = async (req: Request, res: Response) => {
-    try {
-        const payLoad = req.body;
+    const user = await userServices.registerUserIntoDB(payLoad);
 
-        const user = await userServices.registerUserIntoDB(payLoad);
-
-        return sendResponse(res, status.CREATED, true, "User Registratation Successfull", user);
-
-    } catch (err) {
-
-        console.error(err);
-
-        const errorMessage = err instanceof Error ? err.message : "Something went wrong";
-        return sendResponse(res, status.INTERNAL_SERVER_ERROR, false, errorMessage);
-
-    }
-}
+    return sendResponse2(res, {
+        success : true,
+        statusCode : status.CREATED,
+        message : "User Registratation Successfull",
+        data : user
+    });
+})
 
 
 
@@ -46,3 +40,42 @@ const userController = {
 }
 
 export default userController;
+
+
+
+
+
+// const registerUser = async (req: Request, res: Response) => {
+//     try {
+//         const payLoad = req.body;
+
+//         const user = await userServices.registerUserIntoDB(payLoad);
+
+//         return sendResponse(res, status.CREATED, true, "User Registratation Successfull", user);
+
+//     } catch (err) {
+
+//         console.error(err);
+
+//         const errorMessage = err instanceof Error ? err.message : "Something went wrong";
+//         return sendResponse(res, status.INTERNAL_SERVER_ERROR, false, errorMessage);
+
+//     }
+// }
+
+
+// const getAllUser = async (req: Request, res: Response) => {
+//    try{
+
+//         const users = await userServices.getAllUsersFromDB();
+
+//         return sendResponse(res, status.OK, true, "User Information fetched successfully", users);
+
+//    }catch(err){
+    
+//     console.error(err);
+//     const errorMessage = err instanceof Error ? err.message : "Something went wrong";
+//     return sendResponse(res, status.INTERNAL_SERVER_ERROR, false, errorMessage);
+
+//    }
+// }
