5759afa09f0321bd8b985bc6a3d803f3353658fa
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-25 01:26:39 +0600

    added the access and the refresh Token in the cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../../utility/catchAsync";
-import userServices from "../user/user.services";
 import authServices from "./auth.service";
 import sendResponse2 from "../../utility/sendResponse2";
 import status from "http-status";
 
 const loginUser = catchAsync(async(req : Request, res : Response, next : NextFunction)=>{
     const payLoad = req.body;
-    const user = await authServices.loginUserInDB(payLoad);
+    const {accessToken, refreshToken} = await authServices.loginUserInDB(payLoad);
+
+
+    //? setting the access token into the cookies
+    res.cookie("accessToken", accessToken, {
+        httpOnly : true,
+        secure : false,
+        sameSite : "none",
+        maxAge : 1000 * 60 * 60 * 24 
+    })
+
+
+    //? setting the refresh token into the cookie
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly : true,
+        secure : false,
+        sameSite : "none",
+        maxAge : 1000 * 60 * 60 * 24 * 7 
+    })
 
     sendResponse2(res, {
         success : true,
         statusCode : status.OK,
         message : "User Logged In Successfully!",
-        data : user
+        data : {
+            accessToken,
+            refreshToken
+        }
     })
 })
 
