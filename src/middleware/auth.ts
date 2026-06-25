66d27ffd28c7cd55215cf2f4c3e30bb3fa99d15b
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-25 18:52:50 +0600

    verified the access Token, required roles if the user exist and setting the payload

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,25 @@
 import { NextFunction, Request, Response } from "express"
 import catchAsync from "../utility/catchAsync"
 import { Role } from "../../prisma/generated/prisma/enums";
+import { jwtUtility } from "../utility/jwt";
+import { JwtPayload } from "jsonwebtoken";
+import sendResponse2 from "../utility/sendResponse2";
+import status from "http-status";
+import { prisma } from "../lib/prisma";
+
+
+declare global {
+    namespace Express {
+        interface Request {
+            user ? : {
+                id : string;
+                name : string;
+                email : string;
+                role : string;
+            }
+        }
+    }
+}
 
 
 const auth  = (...requiredRoles : Role[])=>{
@@ -16,7 +35,44 @@ const auth  = (...requiredRoles : Role[])=>{
             throw new Error("You are not logged in , kindly login first!");
         }
 
-        const verifiedToken = 
+        //? verifying the token
+        const verifiedToken = jwtUtility.verifyToken(token);
+
+        if(!verifiedToken.success){
+            throw new Error(verifiedToken.errMessage);
+        }
+
+        const {id, name, email, role} = verifiedToken.data as JwtPayload;
+       
+
+        if(requiredRoles && !requiredRoles.includes(role)){
+            sendResponse2(res, {
+                success : false,
+                statusCode : status.FORBIDDEN,
+                message : "Forbidden access, you do not have the permission to access this"
+            })
+        }
+
+        const user = await prisma.user.findUnique({
+            where : {
+                email,
+                id
+            },
+            omit : {
+                password : true
+            }
+        })
+
+        if(!user){
+            throw new Error("User does not Exist!");
+        }
+
+        req.user = {
+            id,
+            name,
+            email,
+            role
+        }
 
         next();
     })
