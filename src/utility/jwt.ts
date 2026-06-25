ad6716acae73be86a2c521fd349b35c3ebf3fef4
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-25 18:44:19 +0600

    added the function for verifying the token

--- src/utility/jwt.ts
+++ src/utility/jwt.ts
@@ -1,5 +1,6 @@
 import { JwtPayload, SignOptions } from "jsonwebtoken"
 import jwt from "jsonwebtoken"
+import config from "../config/config";
 
 const generateToken = (payLoad : JwtPayload, secret : string, expiresIn : SignOptions)=>{
 
@@ -7,6 +8,27 @@ const generateToken = (payLoad : JwtPayload, secret : string, expiresIn : SignOp
     return token;
 }
 
+const verifyToken = (token : string) =>{
+    try{
+
+        const verifyToken = jwt.verify(token, config.jwt_secret);
+
+        return {
+            success : true,
+            message : "Token verified successfully!",
+            data : verifyToken
+        }
+
+    }catch(err){
+        const errMessage = err instanceof Error ? err.message : "Something went wrong";
+        return {
+            success : false,
+            errMessage
+        }
+    }
+}
+
 export const jwtUtility = {
-    generateToken
+    generateToken,
+    verifyToken
 }
