7981139d6a2889729814ac034a8d96d4eb40ff28
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-07 21:02:28 +0600

    getting the stripe customer id if exist , if not therefore creatingg stripe customer id

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,23 +1,39 @@
 import { prisma } from "../../lib/prisma"
+import stripe from "../../lib/stripe";
 
-const createCheckOutSession = async(userId : string)=>{
+const createCheckOutSession = async (userId: string) => {
 
-    const transactionResult = await prisma.$transaction(async(tx)=>{
+    const transactionResult = await prisma.$transaction(async (tx) => {
 
         const user = await tx.user.findUnique({
-            where : {
-                id : userId
+            where: {
+                id: userId
             },
-            include : {
-                subscription : true
+            include: {
+                subscription: true
             }
         })
 
-        if(!userId){
+        if (!userId) {
             throw new Error("User does not Exists");
         }
 
-        // let stripeCustomerId = user?.subscription 
+        //? getting the stripe customer id if exist , if not therefore creatingg stripe customer id
+        //? for old subscriber
+        let stripeCustomerId = user?.subscription?.stripe_customer_id;
+
+        if (!stripeCustomerId) {
+            //? for the new subscriber
+            let customer = await stripe.customers.create({
+                email: user?.email,
+                name: user?.name,
+                metadata: {
+                    userId: user?.id!
+                }
+            });
+
+            stripeCustomerId = customer.id;
+        }
 
     })
 
